bd74999cba4f6a1d4d6486982f3069ff04bdcc61
Ahmad Tayasnh <engahmadtayansa@gmail.com>
2024-12-13 06:50:35 +0200

    finish logic

--- lib/views/pages/search_page.dart
+++ lib/views/pages/search_page.dart
@@ -1,10 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:weather_app_new/services/service_weather.dart';
 
 // ignore: must_be_immutable
 class SearchPage extends StatelessWidget {
-
-
   String? cityName;
 
   SearchPage({super.key});
@@ -16,20 +15,22 @@ class SearchPage extends StatelessWidget {
         title: const Text("Search a City"),
         // backgroundColor: Colors.orange,
       ),
-      body:  Center(
+      body: Center(
         child: Padding(
-          padding:  const EdgeInsets.symmetric(horizontal: 16),
+          padding: const EdgeInsets.symmetric(horizontal: 16),
           child: TextField(
             onSubmitted: (data) {
               cityName = data;
+              ServicesWeather services = ServicesWeather();
+              services.getWeather(cityName: cityName!);
             },
             decoration: const InputDecoration(
-              hintText: "Enter a City",
-              border: OutlineInputBorder(),
-              label: Text("search"),
-              suffixIcon: Icon(Icons.search),
-              contentPadding: EdgeInsets.symmetric(vertical: 32 , horizontal: 24)
-            ),
+                hintText: "Enter a City",
+                border: OutlineInputBorder(),
+                label: Text("search"),
+                suffixIcon: Icon(Icons.search),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 32, horizontal: 24)),
           ),
         ),
       ),
